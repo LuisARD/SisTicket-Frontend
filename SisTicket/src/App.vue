9aa27cf07914cb4e93aa5f2fb83f3dc873b05f55
Luis Angel De La Cruz Castro <130418030+LuisARD@users.noreply.github.com>
2025-12-29 13:31:44 -0400

    Init del proyecto

---
+++ SisTicket/src/App.vue
@@ -0,0 +1,11 @@
+<script setup></script>
+
+<template>
+  <h1>Bienvenido a mi espacio!</h1>
+  <p>
+    Visita <a href="https://luisADelaCruzCastro.org/" target="_blank" rel="noopener">luisADelaCruzCastro.org</a> mi contacto para informarte mejor de como este proyecto
+    funciona cuando no deberia
+  </p>
+</template>
+
+<style scoped></style>
